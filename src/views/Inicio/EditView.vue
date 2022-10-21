3704e46bf28b3f40fb4b103d5f1c41895805dca3
Manuel Diaz <Mana.diaz@duocuc.cl>
2022-10-21 17:50:30 -0300

    Consumiendo Json dede Front

--- src/views/Inicio/EditView.vue
+++ src/views/Inicio/EditView.vue
@@ -2,36 +2,33 @@
     <!-- Cuerpo de la Page -->
     <body class="color_back1_edit">
         <!-- Lo que esta haciendo el Container XL es centrar el div principal, abarcando width: 100%; margin-right: auto; margin-left: auto; -->
-        <div class="container-xl containerxl_padding d-flex" >
+        <div class="containerxl_padding d-flex" >
          <div class="color_back2_edit">
             <div class="focused">
                 <div class="color_top_edit">
-                    <h4> ID: 001 Transcripción Call Center: Falabella. Ejecutivo: Jose Perez Fecha: 10/10/22 </h4>
+                    <h4> ID: 001 Transcripción Call Center: Ripley. Ejecutivo: Felipe Morales Fecha: 25/08/2022 </h4>
                 </div>
                 <div class="row">
                 <div class="col-6"> <!-- Inicio div para separar la pagina -->
-                    <div class="" v-for="c in [1,2,3,4]" v-bind:key="c.id">
+                    <div class="" v-for="lista in listar" v-bind:key="lista">
                 <div class="linea"></div>
                 <div class="focused color_speaker_edit1">
                   <div>
                        <!--  <v-img contain alt="Alloxentric Agent" src="@/assets/Agent.png" max-height="50"></v-img> --> 
-                        <p class="p text-left"> <strong>Speaker0</strong></p> <!-- Se utiliza strong para dejar en negrita el texto -->
-                        <p class="p text-left">
-                            Thank you for calling ABC Company. My name is Ashley. How may I help you today?
-                        </p>
+                        <p class="p text-left"> Speaker : <strong>{{ lista.speaker }}</strong></p> <!-- Se utiliza strong para dejar en negrita el texto -->
+                        <p class="p text-left">{{ lista.transcript }}</p>
+                            
                     </div>
                 </div>
                  </div> 
                 </div> <!-- Fin div para separar la pagina -->
                 <div class="col-6"><!-- Inicio div para separar la pagina -->
-                 <div class="" v-for="c in [1,2,3,4]" v-bind:key="c.id">
+                 <div class="" v-for="Edit in editar" v-bind:key="Edit">
                 <div class="linea"></div>
-                <div class="focused color_speaker_edit1 rounded-xl">
-                    <div class="form-group">
-                       <!--  <v-img contain alt="Alloxentric Agent" src="@/assets/Agent.png" max-height="50"></v-img> --> 
-                        <p class="p text-left"> <input placeholder="Speaker0"></p> <!-- Se utiliza strong para dejar en negrita el texto -->
-                        <!-- la manera en la que se mantiene de ese tamaño es por el style que defini dentro del textarea -->
-                        <textarea class="col-lg-12" name="comentario" rows="2" cols="40">Thank you for calling ABC Company. My name is Ashley. How may I help you today?</textarea>
+                <div class="focused color_speaker_edit1">
+                    <div>
+                        <p class="p text-left"> Speaker : <input v-model="Edit.speaker"></p> 
+                        <textarea class="col-10" name="comentario" rows="2" cols="40"  v-model="Edit.transcript"></textarea>
                     </div>
                 </div>
                  </div> 
@@ -45,23 +42,188 @@
           <button type="button" class="btn btn-outline-success">Guardar</button>
           </div>
           <audio controls>
-            <source src="@/views/Inicio/FALBCSP_20220907-151705_56958208343_557_175429150-all.mp3" type="audio/ogg">
-            <source src="@/views/Inicio/FALBCSP_20220907-151705_56958208343_557_175429150-all.mp3" type="audio/mpeg">
+            <!-- <source src="@/views/Inicio/FALBCSP_20220907-151705_56958208343_557_175429150-all.mp3" type="audio/ogg">
+            <source src="@/views/Inicio/FALBCSP_20220907-151705_56958208343_557_175429150-all.mp3" type="audio/mpeg"> -->
             Your browser does not support the audio element.
            </audio> 
      </div>
     </div>
     </body>
   </template>
+  <script>
+  import { Component, Vue } from 'vue-property-decorator';
+  export default {
+    data () {
+            return{
+            listar: [
+            {
+            speaker: 0,
+            transcript: "Podría, por favor, con don Alexis As Man,"
+            },
+            {
+            speaker: 0,
+            transcript: "Sí con él."
+            },
+            {
+            speaker: 0,
+            transcript: "Buen día, Alexis."
+            },
+            {
+            speaker: 0,
+            transcript: "Mi nombre es de, y le estoy llamando de la empresa cobro que fueran en cargo de Banco Fa,"
+            },
+            {
+            speaker: 0,
+            transcript: "para seguridad llamada podría estar siendo grabada."
+            },
+            {
+            speaker: 0,
+            transcript: "Es una deuda que tiene con un crédito de consumo"
+            },
+            {
+            speaker: 0,
+            transcript: "de empleados"
+            },
+            {
+            speaker: 0,
+            transcript: "de rest de este año, queremos saber lo usted algunas opciones de pago para hacer de alexis y el motivo por el que no había podido hacerlo, que estuvo que"
+            },
+            {
+            speaker: 0,
+            transcript: "endeudamiento, licencia, baja?"
+            },
+            {
+            speaker: 0,
+            transcript: "No, que se salta de octubre,"
+            },
+            {
+            speaker: 0,
+            transcript: "que tanto de decir. Octubre pagué lo máximo el último de febrero"
+            },
+            {
+            speaker: 0,
+            transcript: "me acuerdo la última que pagué, total."
+            },
+            {
+            speaker: 0,
+            transcript: "Ok, explico. El sistema tiene que salir,"
+            },
+            {
+            speaker: 0,
+            transcript: "Ok, le explico, a la fecha el monto de la deuda,"
+            },
+            {
+            speaker: 0,
+            transcript: "se un solo monto,"
+            },
+            {
+            speaker: 0,
+            transcript: "ya le acelerar las cuotas y este los cuatro millones quinientos cincuenta mil cuatrocientos diecinueve. Este monto originalmente"
+            },
+            {
+            speaker: 0,
+            transcript: "está sobre los casi cinco millones, dice un descuento"
+            },
+            {
+            speaker: 0,
+            transcript: "de casi quinientos mil quedando en cuatro mil"
+            },
+            {
+            speaker: 0,
+            transcript: "espéreme un poquito ver, por favor."
+            }],
+            editar: [
+            {
+            speaker: 0,
+            transcript: "Podría, por favor, con don Alexis As Man,"
+            },
+            {
+            speaker: 0,
+            transcript: "Sí con él."
+            },
+            {
+            speaker: 0,
+            transcript: "Buen día, Alexis."
+            },
+            {
+            speaker: 0,
+            transcript: "Mi nombre es de, y le estoy llamando de la empresa cobro que fueran en cargo de Banco Fa,"
+            },
+            {
+            speaker: 0,
+            transcript: "para seguridad llamada podría estar siendo grabada."
+            },
+            {
+            speaker: 0,
+            transcript: "Es una deuda que tiene con un crédito de consumo"
+            },
+            {
+            speaker: 0,
+            transcript: "de empleados"
+            },
+            {
+            speaker: 0,
+            transcript: "de rest de este año, queremos saber lo usted algunas opciones de pago para hacer de alexis y el motivo por el que no había podido hacerlo, que estuvo que"
+            },
+            {
+            speaker: 0,
+            transcript: "endeudamiento, licencia, baja?"
+            },
+            {
+            speaker: 0,
+            transcript: "No, que se salta de octubre,"
+            },
+            {
+            speaker: 0,
+            transcript: "que tanto de decir. Octubre pagué lo máximo el último de febrero"
+            },
+            {
+            speaker: 0,
+            transcript: "me acuerdo la última que pagué, total."
+            },
+            {
+            speaker: 0,
+            transcript: "Ok, explico. El sistema tiene que salir,"
+            },
+            {
+            speaker: 0,
+            transcript: "Ok, le explico, a la fecha el monto de la deuda,"
+            },
+            {
+            speaker: 0,
+            transcript: "se un solo monto,"
+            },
+            {
+            speaker: 0,
+            transcript: "ya le acelerar las cuotas y este los cuatro millones quinientos cincuenta mil cuatrocientos diecinueve. Este monto originalmente"
+            },
+            {
+            speaker: 0,
+            transcript: "está sobre los casi cinco millones, dice un descuento"
+            },
+            {
+            speaker: 0,
+            transcript: "de casi quinientos mil quedando en cuatro mil"
+            },
+            {
+            speaker: 0,
+            transcript: "espéreme un poquito ver, por favor."
+            }
+
+            ]}
+       }
+
+  }
+ 
+  </script>
   <style>
   textarea{
     /* background-color: white; */
     border: 1px; 
     padding:0px;
     margin:0px;
-    overflow: hidden;
-
-  }
+    overflow: hidden; 
+ }
 /*    Padding que separa los dos colores en TOP */
   .containerxl_padding{
       padding: 15px;
@@ -121,4 +283,3 @@
   
   
   </style>
-  
